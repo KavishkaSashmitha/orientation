9788f40f520a69113b1ac41b8b2365407e17b826
Movindu Liyanage <142730691+Movin21@users.noreply.github.com>
2025-02-12 21:10:41 +0530

    Update AgendaData.ts (Incomplete)

--- src/data/AgendaData.ts
+++ src/data/AgendaData.ts
@@ -1,32 +1,53 @@
 export const AgendaData = [
   {
-    title: "Welcome speech by Ms. Thilini Jayalath",
-    time: "1:30 PM",
-    imageURL: "/faces/msthilini.png",
-    name: "Ms. Thilini Jayalath",
-    position: "Senior Lecturer, SLIIT",
-  },
-  {
-    title: "Speech by Dr. Kalpani Manathunga",
-    time: "1:35 PM",
+    title: "Opening & Speech by Dr. Kalpani Manathunga",
+    time: "1:00 PM",
     imageURL: "/faces/kalpani.png",
     name: "Dr. Kalpani Manathunga",
     position: "HOD, SE Department SLIIT",
   },
   {
+    title: "Staff Introduction",
+    time: "1:10 PM",
+  },
+  {
     title: "Speech by Prof. Nuwan Kodagoda",
-    time: "1:50 PM",
+    time: "1:15 PM",
     imageURL: "/faces/nuwan.png",
     name: "Prof. Nuwan Kodagoda",
     position: "Pro Vice Chancellor, SLIIT",
   },
   {
-    title: "Staff Introduction",
-    time: "2:20 PM",
+    title: "Address by Prof. Pradeep Abeygunawardhana",
+    time: "1:30 PM",
+    imageURL: "/faces/", //face needs to be changed
+    name: " Prof. Pradeep Abeygunawardhana",
+    position: "Dean, Faculty Of Computing, SLIIT",
   },
   {
     title: "Entertainment Session",
-    time: "2:30 PM",
+    time: "1:40 PM",
+  },
+  /*{
+    title: "Speech by Mr. Ashan Priyadarshana",
+    time: "1:45 PM",
+    imageURL: "/faces/", //face needs to be changed
+    name: "Mr. Ashan Priyadarshana",
+    position: "Tech Lead, Sysco Labs",
+  },*/
+  {
+    title: "Speech by Ms. Madhumini Gunaratne",
+    time: "2:05 PM",
+    imageURL: "/faces/", //face needs to be changed
+    name: "Ms. Madhumini Gunaratne",
+    position: "Research Assistant, Faculty Of Computing, SLIIT",
+  },
+  {
+    title: "Speech by Mr. Ashan Priyadarshana",
+    time: "2:20 PM",
+    imageURL: "/faces/", //face needs to be changed
+    name: "Mr. Ashan Priyadarshana",
+    position: "Tech Lead, Sysco Labs",
   },
   {
     title: "Speech by Mr. Saman Gunawardena",
@@ -36,33 +57,30 @@ export const AgendaData = [
     position: "Senior Manager Industrial Liaison and Placement",
   },
   {
-    title: "Speech by Mr. Danushka Ranasinghe",
-    time: "3:10 PM",
-    imageURL: "/faces/danushka.png",
-    name: "Mr. Danushka Ranasinghe",
-    position: "Senior IFS Technical Consultant",
-  },
-  {
-    title: "Speech by Perituza Lanka",
-    time: "3:40 PM",
-  },
-  {
     title: "Speech by Mr. Pasindu Dinal",
-    time: "4:00 PM",
+    time: "2:55 PM",
     imageURL: "/faces/dinal.png",
     name: "Pasindu Dinal",
     position: "President, SESC 2024/25",
   },
   {
-    title: "Speech by President of FCSC",
-    time: "4:15 PM",
-    imageURL: "/faces/rivi.png",
-    name: "Rivi Abhishek",
-    position: "President, FCSC",
+    title: "Speech by Ms. Subhashini Wijerathna",
+    time: "3:10 PM",
+    imageURL: "/faces/",
+    name: "Ms. Subhashini Wijerathna",
+    position:
+      "Associate Manager in Software Engineering, VitalHub Innovations Lab",
+  },
+  {
+    title: "Speech by Ms. Sandamali Vidanagamage",
+    time: "3:10 PM",
+    imageURL: "/faces/",
+    name: " Ms. Sandamali Vidanagamage",
+    position: "Manager in Software Engineering, VitalHub Innovations Lab",
   },
   {
     title: "Vote of Thanks",
-    time: "4:30 PM",
+    time: "3:40 PM",
     imageURL: "/faces/gokul.png",
     name: "Gokul Abishek",
     position: "Secretary, SESC 2024/25",
